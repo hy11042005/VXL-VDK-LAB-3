86e7791dd0ac67df3678d9b33cfbbe5da90963ea
Hye <minhhynguyen2021@gmail.com>
2025-10-13 12:28:09 +0700

    Add files via upload

---
+++ input_processing.h
@@ -0,0 +1,14 @@
+#ifndef INC_INPUT_PROCESSING_H_
+#define INC_INPUT_PROCESSING_H_
+
+#include "main.h"
+
+extern int mode;
+extern int red_duration;
+extern int yellow_duration;
+extern int green_duration;
+
+void fsm_for_input_processing(void);
+
+#endif /* INC_INPUT_PROCESSING_H_ */
+
